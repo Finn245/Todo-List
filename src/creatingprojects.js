c4e1381958a694580695833d6b20e9adf8f7d363
Hoper <aderahmenmanai@gmail.com>
2024-03-29 15:36:41 +0100

    Added default project array with initial values.

--- src/creatingprojects.js
+++ src/creatingprojects.js
@@ -1,12 +1,9 @@
 // Array to store projects
 const projects = [
-    {
-        name: 'Default Project',
-        tasks: []
-    }
+  {
+    name: 'Default Project',
+    tasks: [],
+  },
 ];
 
 export { projects };
-import { saveToLocalStorage ,loadFromLocalStorage } from "./localStorage"
-/*saveToLocalStorage(projects)
-loadFromLocalStorage();*/
