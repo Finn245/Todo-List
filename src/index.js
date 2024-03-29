a45d34287817128d63bfe9559586a555cd27308e
Hoper <aderahmenmanai@gmail.com>
2024-03-29 15:35:15 +0100

    Add initial setup and loading projects from localStorage

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import "./styles/reset.css";
-import "./styles/style.css";
-import { initializeProjects, initializeEventListeners, renderProject } from "./dommanipulation.js";
-import { projects } from "./creatingprojects.js"; // Importing the projects array
-import greeting from "./images/greeting.svg";
+import './styles/reset.css';
+import './styles/style.css';
+import {
+  initializeProjects,
+  initializeEventListeners,
+  renderProject,
+} from './dommanipulation.js';
+import { projects } from './creatingprojects.js'; // Importing the projects array
+import greeting from './images/greeting.svg';
 import ques from './images/ques.svg';
 
-const main = document.querySelector(".todo-main");
+const main = document.querySelector('.todo-main');
 main.innerHTML = `<div class="faq"><img src="${greeting}" alt=""></div>
 <div class='text'>Greeting! This my todolist application please choose any available projects or create new one so you can add your tasks. </div>
 `;
@@ -14,15 +18,13 @@ renderProject();
 initializeProjects();
 initializeEventListeners();
 
-import { saveToLocalStorage, loadFromLocalStorage } from "./localStorage";
+import { saveToLocalStorage, loadFromLocalStorage } from './localStorage';
 
 // Load projects from localStorage
 const loadedProjects = loadFromLocalStorage();
 
 // Check if there are projects loaded from localStorage
 if (loadedProjects.length > 0) {
-    // Replace projects array with loaded projects
-    projects.splice(0, projects.length, ...loadedProjects);
+  // Replace projects array with loaded projects
+  projects.splice(0, projects.length, ...loadedProjects);
 }
-
-console.log(projects);
